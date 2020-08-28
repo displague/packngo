2905f4633408a8cc20f4347233e631cc857b2e0c
Marques Johansson <marques@packet.com>
2020-08-28 00:56:54 -0400

    add mock API tests for *ServiceOp methods
    These mocks make it easy to quickly test the logic in these methods
    and in the supporting functions without making a full API call.
    
    In this commit, ProjectServiceOp.ListSSHKeys is tested by mocking the
    client with one that is only concerned with providing the expected
    object or error.
    
    Signed-off-by: Marques Johansson <marques@packet.com>

--- packngo_test.go
+++ packngo_test.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/hashicorp/go-retryablehttp"
 )
 
 const (
@@ -34,6 +36,36 @@ func randString8() string {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 	return string(b)
+}
+
+// MockClient makes it simpler to test the Client
+type MockClient struct {
+	fnNewRequest          func(method, path string, body interface{}) (*retryablehttp.Request, error)
+	fnDo                  func(req *retryablehttp.Request, v interface{}) (*Response, error)
+	fnDoRequest           func(method, path string, body, v interface{}) (*Response, error)
+	fnDoRequestWithHeader func(method string, headers map[string]string, path string, body, v interface{}) (*Response, error)
+}
+
+var _ requestDoer = &MockClient{}
+
+// NewRequest uses the mock NewRequest function
+func (mc *MockClient) NewRequest(method, path string, body interface{}) (*retryablehttp.Request, error) {
+	return mc.fnNewRequest(method, path, body)
+}
+
+// Do uses the mock Do function
+func (mc *MockClient) Do(req *retryablehttp.Request, v interface{}) (*Response, error) {
+	return mc.fnDo(req, v)
+}
+
+// DoRequest uses the mock DoRequest function
+func (mc *MockClient) DoRequest(method, path string, body, v interface{}) (*Response, error) {
+	return mc.fnDoRequest(method, path, body, v)
+}
+
+// DoRequestWithHeader uses the mock DoRequestWithHeader function
+func (mc *MockClient) DoRequestWithHeader(method string, headers map[string]string, path string, body, v interface{}) (*Response, error) {
+	return mc.fnDoRequestWithHeader(method, headers, path, body, v)
 }
 
 func setupWithProject(t *testing.T) (*Client, string, func()) {
